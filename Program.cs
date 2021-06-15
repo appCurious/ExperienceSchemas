197142b0d4643cb157924bc4818a9b883912b199
gregclark <greg.clark@syndigo.com>
2021-06-15 14:56:03 -0500

    working to produce a console for the reason schema is invalid

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ExperienceSchemas
                 ""items"":[
                     {
                         ""caption"":""I have a bullet point and a footnote"",
-                        ""description"":""<p>CXH does not send in the quill indent css</p><ul><li>single bullet </li></ul><p>foot note<markup:footnote xmlns:markup=http://www.webcollage.com/objects/2008/1/markup><markup:rich-text>first footnote</markup:rich-text></markup:footnote></p>"",
+                        ""description"":""<p>CXH does not send in the quill indent css</p><ul><li>single bullet </li></ul><p>foot note<markup:footnote xmlns:markup=http://www.wherever.com/objects/2008/1/markup><markup:rich-text>first footnote</markup:rich-text></markup:footnote></p>"",
                         ""link"": null,
                         ""linkLabel"":""Learn More"",
                         ""assetType"":""Video"",
@@ -26,7 +26,7 @@ namespace ExperienceSchemas
                             ""duration"":183.318005,
                             ""poster"":{
                                 ""assetType"":""Image"",
-                                ""url"":""https://alt-uat.syndigo.cloud/asset/a32e15eb-7a80-4ac9-9f47-61e56ad00c77/{0}.jpeg"",
+                                ""url"":""https://wherever.cloud/asset/a32e15eb-7a80-4ac9-9f47-61e56ad00c77/{0}.jpeg"",
                                 ""alt"":"""",
                                 ""assetId"":""be9b915d-fe7f-404a-807b-b1ea5340349f"",
                                 ""originalHeight"":1529,
@@ -43,13 +43,13 @@ namespace ExperienceSchemas
                                 {
                                     ""kind"":""captions"",
                                     ""srcLang"":""en"",
-                                    ""url"":""https://alt-uat.syndigo.cloud/asset/c41cfdd3-7241-4e47-8f5e-bbe80e724004/original.vtt""
+                                    ""url"":""https://wherever.cloud/asset/c41cfdd3-7241-4e47-8f5e-bbe80e724004/original.vtt""
                                 }
                             ],
                             ""sources"":[
                                 {
                                     ""mimeType"":""video/hls"",
-                                    ""url"":""https://alt-uat.syndigo.cloud/asset/af79624a-1739-4e11-b823-aa7e85b835ac/playlist.m3u8""
+                                    ""url"":""https://wherever.cloud/asset/af79624a-1739-4e11-b823-aa7e85b835ac/playlist.m3u8""
                                 }
                             ],
                             ""assetId"":""af79624a-1739-4e11-b823-aa7e85b835ac"",
@@ -65,7 +65,7 @@ namespace ExperienceSchemas
                         ""fontBackgroundColor"":""#ffffff"",
                         ""fontColor"":""#000000"",
                         ""imageBackgroundColor"":null,
-                        ""fontOpacity"":100,
+                        ""fontOpacity"":""blue"",
                         ""textWidth"":100,
                         ""textPosition"":""Center""
                     }
@@ -81,8 +81,17 @@ namespace ExperienceSchemas
             Console.Write(results.IsValid);
             Console.WriteLine("");
             Console.Write("Result Messages: ");
-            var message = results.Message != null ? results.Message.ToString() : "Valid JSON";
-            Console.Write(message);
+            string message = results.Message != null ? results.Message : "Valid JSON";
+            if (results.NestedResults != null)
+            {
+                // iterate over the list of ValidationResults
+                // it might be the Message is only for true Exceptions
+                foreach (ValidationResults r in results.NestedResults) {
+                    message = message + "\n" + r.Message;
+                }
+            }
+            results.ToBasic();
+            Console.Write(results.Message);
         }
     }
 }
